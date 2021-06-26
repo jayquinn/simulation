1aecf0850f93e6c2cf28bd53357cbe9d673e5a06
Hyeon-jong Jay Yu <61223949+jayquinn@users.noreply.github.com>
2021-06-26 16:59:05 +0900

    osoi

--- K-BNT replicate.R
+++ K-BNT replicate.R
@@ -1,1 +1,10 @@
-#hi!
+####K-BNT replicate
+#자비스 사람만들기
+id<-1:1953
+age<-rep(1:9,each=217) # 9, 217
+edu<-rep(1:7,each=279) # 7, 279
+mean<-40.35-0.23*(age^2)+8.25*log(edu)+0.05*(age^2)*log(edu)
+variance<-108.9*exp(-0.01*age-0.43*edu+0.04*age*edu)
+sd<-sqrt(variance)
+sample<-data.frame(id,age,edu,mean,sd)
+edit(sample)
